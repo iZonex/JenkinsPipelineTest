f29934ee80a7f80ec8f865966f20b21ce8b5a99d
Dmytro Chystiakov <dmytro.chystiakov@globallogic.com>
2018-06-28 14:05:17 +0300

    fix function

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ async def fetch_from_source(loop):
 def main():
     """ start main function """
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(go(loop))
+    loop.run_until_complete(fetch_from_source(loop))
     loop.close()
 
 
